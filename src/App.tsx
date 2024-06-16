b6709e7d09c90cebcc75e81961b51f9a2b8a65b2
kate-codes-ui <n3morosa@gmail.com>
2024-06-16 15:53:14 +0200

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Container from './Container';
+import Footer from './Footer';
+
+
+function App() {
+
+  return (
+    <>
+      <Container />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
